725f7db3659f7ec3e292bf37d933bf4f120fcb13
dimaserbenyuk <dimaserbenyuk@users.noreply.github.com>
2024-07-26 16:48:50 +0000

    deploy: 8a7f685c27dff8148a756614d743c53bfae5d1ef

--- authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-   path("auth/google/", views.GoogleLoginApi.as_view(), 
-         name="login-with-google"),
-]
--- voicengerdb/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    name = models.CharField(max_length=140)
-    nickname = models.CharField(max_length=50)
-    age = models.IntegerField()
