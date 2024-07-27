338ab9d361dcabc0fd697aa3b907b70154a147af
Raigester <vovaraigester@gmail.com>
2024-07-27 16:39:02 +0300

    Add login logut

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
    path("auth/google/", views.GoogleLoginApi.as_view(), 
          name="login-with-google"),
+   path('accounts/', include('allauth.urls')),
 ]
