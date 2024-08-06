9ac4e38ec554b16e59ecc644aa6dad059db568d8
AntonPrime11 <129660367+AntonPrime11@users.noreply.github.com>
2024-08-06 18:42:43 +0300

    Add Views, Serializers.

---
+++ voicengerdb/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import User, Chat, ChatParticipant, Message, MessageReadReceipt
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chat
+        fields = '__all__'
+
+
+class ChatParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatParticipant
+        fields = '__all__'
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+
+class MessageReadReceiptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MessageReadReceipt
+        fields = '__all__'
---
+++ voicengerdb/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, ChatViewSet, ChatParticipantViewSet, MessageViewSet, MessageReadReceiptViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'chats', ChatViewSet)
+router.register(r'chat-participants', ChatParticipantViewSet)
+router.register(r'messages', MessageViewSet)
+router.register(r'message-read-receipts', MessageReadReceiptViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
