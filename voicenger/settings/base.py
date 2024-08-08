8d0e2c338ccadeb693ab2f68397565b09ae0ed27
Dmytro Serbeniuk <dmytro.serbeniuk@lotusflare.com>
2024-08-08 14:37:14 +0200

    add

--- voicenger/settings/base.py
+++ voicenger/settings/base.py
@@ -191,11 +191,11 @@ SOCIALACCOUNT_PROVIDERS = {
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'   
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #For WebSockets
 CHANNEL_LAYERS = {
--- voicenger/urls.py
+++ voicenger/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('test/', test_page, name='test_page'),
-
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Serves media files during development
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
