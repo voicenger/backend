354e1fd9222cdd04fc79f4e488062b49bb47c378
Dmitriy Serbeniuk <dserbenyukgood@gmail.com>
2024-08-10 14:44:14 +0200

    fix: Create first releases backend

--- voicenger/settings/base.py
+++ voicenger/settings/base.py
@@ -102,9 +102,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'   
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Password validation
-# https://docs.djangoproject.com/en/<version>/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
