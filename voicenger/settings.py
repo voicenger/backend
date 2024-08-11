3eb442fd5c92218e5edb6050de325e44b6565364
Raigester <vovaraigester@gmail.com>
2024-08-11 18:09:58 +0300

    Start

--- voicenger/settings.py
+++ voicenger/settings.py
@@ -93,12 +93,10 @@ WSGI_APPLICATION = 'voicenger.wsgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
+
 
 
 MEDIA_URL = '/media/'
