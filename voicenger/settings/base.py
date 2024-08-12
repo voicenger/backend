a3a02b64c542964843ef99f7203b10144464e745
Raigester <vovaraigester@gmail.com>
2024-08-12 10:38:50 +0300

    fix

--- voicenger/settings/base.py
+++ voicenger/settings/base.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/<version>/ref/settings/#default-auto-field 
+# https://docs.djangoproject.com/en/<version>/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
