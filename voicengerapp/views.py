cc960592fcbe5da1520ffc1553cf03baa6c2ea5b
AntonPrime11 <129660367+AntonPrime11@users.noreply.github.com>
2024-08-14 10:24:35 +0300

    Add message deletion functionality
    Implemented the `destroy` and `perform_destroy` methods in the view to enable message deletion. This change allows users to delete messages, providing a confirmation response upon successful deletion.

--- voicengerapp/views.py
+++ voicengerapp/views.py
@@ -85,12 +85,19 @@ class MessageViewSet(viewsets.ModelViewSet):
         user_chats = Chat.objects.filter(participants=user)
         return Message.objects.filter(chat__in=user_chats)
 
-    # Returns the current user's messages in a specific chat (by chat ID).
     def user_chat_messages(self, request, id=None):
         chat_id = id
         messages = Message.objects.filter(chat_id=chat_id)
         serializer = self.get_serializer(messages, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"detail": "Message successfully deleted"}, status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
 
 
 class UserChatViewSet(viewsets.ModelViewSet):
