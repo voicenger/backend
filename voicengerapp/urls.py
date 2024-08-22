414943cb6d7a24db52ed062af2076c7d635c06cf
Raigester <vovaraigester@gmail.com>
2024-08-22 17:02:45 +0300

    [CHAT-73] "Front Server Side Authentication and Authorization". Remake auth0, upgrade custom jwt token verifier, add taking access token from auth0, add function login, logout, callback

--- voicengerapp/urls.py
+++ voicengerapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import ChatViewSet, MessageViewSet, UserChatViewSet, RegisterView, index, logout, profile
+from .views import ChatViewSet, MessageViewSet, UserChatViewSet, RegisterView, logout, auth0_callback, login_redirect
 
 router = DefaultRouter()
 router.register(r'chats', ChatViewSet)
@@ -14,9 +14,10 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('app/', index,name='index'),
-    path('app/profile/',profile,name='profile'),
     path('app/',include('social_django.urls')),
     path('app/logout/',logout,name='logout'),
     path('messages/chat/<int:id>/', MessageViewSet.as_view({'get': 'user_chat_messages'}), name='user_chat_messages'),
+    path('login/', login_redirect, name='login_redirect'),
+    path('logout/', logout, name='logout'),
+    path('callback/', auth0_callback, name='auth0_callback')
 ]
